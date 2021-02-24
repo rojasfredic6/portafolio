7897b00b5ce36cd692994e900bdad6b53e36b45e
pitufredic <rojas.fredic6@gmail.com>
2021-02-24 18:34:07 -0500

    modificando iconos

--- templates/Footer.js
+++ templates/Footer.js
@@ -1,10 +1,22 @@
 const Footer = () => {
     const view = `
         <div class="flex items-center flex-col md:flex-row">
-          <p class="flex-1 p-1">Email: <span class="bg-white text-gray-700 p-1.5 rounded"> rojas.fredic6@gmail.com </span> </p>
-          <p class="flex-1 p-1">Cellphone: <span class="bg-white text-gray-700 p-1.5 rounded"> 3106725629 </span></p>
-          <p class="flex-1 p-1">Github: <span class="bg-white text-gray-700 p-1.5 rounded"> https://github.com/rojasfredic6 </span></p>
-          <p class="flex-1 p-1">LinkedIn: <span class="bg-white text-gray-700 p-1.5 rounded"> <a href="">https://www.linkedin.com/in/fredic-yohany-rojas-parra-65ba8012b/</a></span></p>
+          <div class="flex flex-1 flex-col items-center">
+            <p class="p-1">Email:</p>
+            <p class="bg-white text-gray-700 p-1.5 rounded"> rojas.fredic6@gmail.com </p>
+          </div>
+          <div class="flex flex-1 flex-col items-center">
+            <p class="p-1">Cellphone:</p>
+            <p class="bg-white text-gray-700 p-1.5 rounded"> 3106725629 </p>
+          </div>
+          <div class="flex flex-1 flex-col items-center">
+            <p class="p-1">Github: </p>
+            <p class="bg-white text-gray-700 p-1.5 rounded"> <a href="https://github.com/rojasfredic6"> https://github.com/rojasfredic6 </a></p>
+          </div>
+          <div class="flex flex-1 flex-col items-center">
+            <p class="p-1">LinkedIn: </p>
+            <p class="bg-white text-gray-700 p-1.5 rounded"> <a href="https://www.linkedin.com/in/fredic-yohany-rojas-parra-65ba8012b/">https://www.linkedin.com/in/fredic-yohany-rojas-parra-65ba8012b/</a></p>
+          </div>
         </div>
     `
     return view;
